2602083e4fe7db63a630f4edcd533c602bd8731c
blancoparis-tfc <blancoparis.tfc@gmail.com>
2015-01-31 22:55:42 +0100

    Plantilla base de gradle
    Montar los ficheros basicos,  para hacer los git.

---
+++ core/src/org/dbp/util/arboles/core/NodoGenericoDTO.java
@@ -0,0 +1,17 @@
+package org.dbp.util.arboles.core;
+
+import javafx.scene.control.TreeItem;
+
+import java.io.Serializable;
+
+/**
+ *
+ * Es el nodo que vamos a usar, para poder trabajar con los nodos.
+ *
+ * Created by david on 01/31/15.
+ */
+public class NodoGenericoDTO<T extends Serializable> implements Serializable {
+
+
+
+}

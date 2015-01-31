bafd87319cc9a1960ce2fa6bf1d98bc61d935cb8
blancoparis-tfc <blancoparis.tfc@gmail.com>
2015-02-01 00:09:34 +0100

    arbolesUtils-2 Creación de la estructura comun
      Añadido los datos

---
+++ src/test/java/org/dbp/util/arboles/core/PruebasJavaFxTest.java
@@ -0,0 +1,15 @@
+package org.dbp.util.arboles.core;
+
+import org.junit.Test;
+
+/**
+ * Created by david on 01/31/15.
+ */
+public class PruebasJavaFxTest {
+
+    @Test
+    public void eliminar(){
+        System.out.println("Eco");
+    }
+
+}

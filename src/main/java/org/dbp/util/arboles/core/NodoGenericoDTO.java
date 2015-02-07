e88520e4425e17fec1680ec23bcb42e5f9369a40
blancoparis-tfc <blancoparis.tfc@gmail.com>
2015-02-07 22:42:34 +0100

    Cierre de la tarea:
    https://github.com/blancoparis-tfc/arbolesUtils/issues/2
    
    Se ha creado la estrucutura general de trabajo.

--- src/main/java/org/dbp/util/arboles/core/NodoGenericoDTO.java
+++ src/main/java/org/dbp/util/arboles/core/NodoGenericoDTO.java
@@ -1,17 +1,75 @@
 package org.dbp.util.arboles.core;
 
-import javafx.scene.control.TreeItem;
-
 import java.io.Serializable;
+import java.util.List;
 
 /**
  *
  * Es el nodo que vamos a usar, para poder trabajar con los nodos.
- *
+ * 
+  * <ul>
+ * <li>
+ * <T> valor: Los datos</li>
+ * <li>
+ * <ID> id: El identificador donde vamos a poder ir abuscar.</li>
+ * <li>
+ * NodoGenericoDTO<T,ID> padre: Un pentero al padre.</li>
+ * <li>
+ * hijos: Los hijos.</li>
+ * </ul>
  * Created by david on 01/31/15.
  */
-public class NodoGenericoDTO<T extends Serializable> implements Serializable {
+@SuppressWarnings("serial")
+public class NodoGenericoDTO<T extends Serializable, ID extends Serializable>
+		implements Serializable {
 
+	/**
+	 * Es el valor con el que vamos a trabajar.
+	 */
+	private T valor;
+	/**
+	 * Es el id, para poder identifcador.
+	 */
+	private ID id;
+	/**
+	 * Es un puntero al padre, como un puntero.
+	 */
+	private NodoGenericoDTO<T, ID> padre;
+	/**
+	 * La lista de los hijos.
+	 */
+	private List<NodoGenericoDTO<T, ID>> hijos;
 
+	public T getValor() {
+		return valor;
+	}
+
+	public void setValor(T valor) {
+		this.valor = valor;
+	}
+
+	public ID getId() {
+		return id;
+	}
+
+	public void setId(ID id) {
+		this.id = id;
+	}
+
+	public NodoGenericoDTO<T, ID> getPadre() {
+		return padre;
+	}
+
+	public void setPadre(NodoGenericoDTO<T, ID> padre) {
+		this.padre = padre;
+	}
+
+	public List<NodoGenericoDTO<T, ID>> getHijos() {
+		return hijos;
+	}
+
+	public void setHijos(List<NodoGenericoDTO<T, ID>> hijos) {
+		this.hijos = hijos;
+	}
 
 }
